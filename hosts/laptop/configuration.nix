1d30427797b3ae4f932a1eb8bdf5ea820f550c30
RaftDev <theraft08@gmail.com>
2025-12-08 18:30:29 +0100

    Removed broken ass windows boot entry

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -34,16 +34,6 @@
       device = "nodev";
       useOSProber = true;
       efiSupport = true;
-      extraEntries = ''
-        menuentry "Windows" {
-          insmod part_gpt
-          insmod fat
-          insmod search_fs_uuid
-          insmod chain
-          search --fs-uuid --set=root $FS_UUID
-          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-        }
-      '';
     };
 
     efi = {
