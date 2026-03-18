b6f579adc01eeefe887d3a047f37aa5564d2c36f
RaftDev <theraft08@gmail.com>
2026-03-18 23:03:26 +0100

    Disabled wireguard on desktop, because what's the point ?

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -20,7 +20,7 @@
   };
 
   module.service.wireguard = {
-    enable = true;
+    enable = false;
     localAddress = "10.0.0.2/32";
     privateKeyFile = config.sops.secrets."wireguard/desktop/key".path;
   };
